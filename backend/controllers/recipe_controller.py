1742e43c2da01c301fec0b2a3369d74e80c1dc65
cmgvc <chloe.gavv@gmail.com>
2024-11-23 10:56:56 -0500

    more init

---
+++ backend/controllers/recipe_controller.py
@@ -0,0 +1,9 @@
+from flask import request, jsonify
+
+
+def get_recipes():
+    return "get_recipes"    
+
+
+def get_recipe_by_id():
+    return "get_recipe_by_id"
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Fridge from './pages/Fridge';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/fridge" exact component={Fridge} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
