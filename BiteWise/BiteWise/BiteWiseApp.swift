26a97f9f38eedf8b0b1b65a175351af07e4a9940
cmgvc <chloe.gavv@gmail.com>
2024-11-22 21:49:24 -0500

    init xcode

---
+++ BiteWise/BiteWise/BiteWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteWiseApp.swift
+//  BiteWise
+//
+//  Created by Chloe Gavrilovic on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct BiteWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
