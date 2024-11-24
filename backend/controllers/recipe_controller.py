7dcbb4d147615c8c8951d7a2497772b9c9eb38c1
Emily Roest <roestemily@gmail.com>
2024-11-24 02:01:42 -0500

    api update file

--- backend/controllers/recipe_controller.py
+++ backend/controllers/recipe_controller.py
@@ -1,9 +1,60 @@
 from flask import request, jsonify
+import requests
 
+def get_recipes(ingredients_list):
+    """
+    Get top 5 recipes with matching ingredients and return
+    NOTE: does not get recipe info, but rather returns the id numbers to be used to search further
+    Args:
+        ingredients_list (list : str): List of expiring ingredients
+    """
 
-def get_recipes():
-    return "get_recipes"    
+    api_ingredients = ",+".join(ingredients_list)
+    print(api_ingredients)
 
+    url = f"https://api.spoonacular.com/recipes/findByIngredients?apiKey=a6fb9076765142499de63cdfa13fff0a&ingredients={api_ingredients}&number=5"
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    recipes = {recipe['id']: recipe['title'] for recipe in data}
+    print(recipes)
+    # returns a dictionary of recipes, key is id, value is name
+    return recipes
+    
+def get_recipe_by_id(id_num):
+    url = f"https://api.spoonacular.com/recipes/{id_num}/information?apiKey=a6fb9076765142499de63cdfa13fff0a"
+    
+    response = requests.get(url)
+    recipe_data = response.json()
 
-def get_recipe_by_id():
-    return "get_recipe_by_id"
+    # Extract the main recipe details
+    title = recipe_data.get("title", "No Title")
+    servings = recipe_data.get("servings", "N/A")
+    ready_in_minutes = recipe_data.get("readyInMinutes", "N/A")
+    instructions = recipe_data.get("instructions", "No Instructions Provided")
+    ingredients = recipe_data.get("extendedIngredients", [])
+    image_url = recipe_data.get("image", "No Image Available")
+
+    # Format ingredients
+    formatted_ingredients = [
+        {
+            "name": ingredient.get("name", "Unknown Ingredient"),
+            "amount": ingredient.get("amount", ""),
+            "unit": ingredient.get("unit", "")
+        }
+        for ingredient in ingredients
+    ]
+
+    # Prepare the recipe details as a JSON object
+    recipe_details = {
+        "title": title,
+        "servings": servings,
+        "ready_in_minutes": ready_in_minutes,
+        "instructions": instructions,
+        "ingredients": formatted_ingredients,
+        "image_url": image_url
+    }
+    print(recipe_details)
+
+    return recipe_details
